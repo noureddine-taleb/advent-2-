a98e7c302ccdea3fd2dfc7a3ab1804ea45a7826e
Nour-eddine Taleb <contact@noureddine.xyz>
2023-04-05 21:51:06 +0000

    14-getdents
    Signed-off-by: Nour-eddine Taleb <contact@noureddine.xyz>

--- 14-getdents/letters.c
+++ 14-getdents/letters.c
@@ -16,11 +16,22 @@
 // Christmas, we have to know the timestamp of Christmas. We derive
 // that timestamp from the current time.
 time_t christmas_day(int delta_year) {
-    // FIXME: calculate timestamp for Christmas.
     // delta_years = 0  => this Christmas
     // delta_years = -1 => last Christmas
-    return time(NULL);
+    time_t t;
+    time_t year_time = 60 * 60 * 24 * 365;
+    time(&t);
+    t += year_time * delta_year;
+    t = t / year_time * year_time + year_time - 1;
+    return t;
 }
+struct linux_dirent64 {
+    ino64_t        d_ino;    /* 64-bit inode number */
+    off64_t        d_off;    /* 64-bit offset to next structure */
+    unsigned short d_reclen; /* Size of this dirent */
+    unsigned char  d_type;   /* File type */
+    char           d_name[]; /* Filename (null-terminated) */
+};
 
 // For us, a letter consists of a timestamp and a pointer to a
 // filename.
@@ -29,19 +40,61 @@ struct letter {
     char  *filename;
 };
 
+void insert_letter(struct letter **letters, int *len, char *name, time_t ts) {
+    *letters = realloc(*letters, (*len + 1) * sizeof (struct letter));
+    struct letter *letter = &(*letters)[*len];
+    letter->filename = name;
+    letter->timestamp = ts;
 
+    (*len)++;
+}
 
+int cmp_letter(const void *l1, const void *l2) {
+    return ((struct letter *)l1)->timestamp - ((struct letter *)l2)->timestamp;
+}
+
+time_t sec_to_days(time_t time) {
+    return time / 60 / 60 / 24;
+}
+
+void print_time(time_t t) {
+    struct tm * ptm = localtime(&t);
+    char buffer[32];
+    // Format: Mo, 15.06.2009 20:20:00
+    strftime(buffer, 32, "%a, %d.%m.%Y %H:%M:%S\n", ptm);
+    printf(buffer); 
+}
 
 int main(int argc, char *argv[]) {
-    // FIXME: Open the directory with open(2) and O_DIRECTORY
-    // FIXME: Read directory entries with getdents64(2)
-    // FIXME: For each entry, use statx(2) to extract the birthtime
-    // FIXME: Sort by birthtime with qsort(3)
-    // FIXME: Output letters that were created after the last christmas
-    //        and before the next christmas in the following format
-    //        (151 until the next Christmas).
-
-    // $ ./letters
-    // 151 days: Makefile
-    // 151 days: letters
+    int dir = open(".", O_RDONLY|O_DIRECTORY);
+    if (dir < 0)
+        die("open");
+    char buf[1024];
+    int ret;
+    struct statx sbuf;
+    time_t last_christmas = christmas_day(-1);
+    time_t next_christmas = christmas_day(0);
+    // printf ("last chris ="); print_time(last_christmas);
+    // printf ("next chris ="); print_time(next_christmas);
+    struct letter *letters = NULL;
+    int letters_len = 0;
+    struct linux_dirent64 *dent;
+    while ((ret = getdents64(dir, buf, sizeof buf)) > 0) {
+        for (
+            unsigned long off = 0;
+            off < ret && (dent = (struct linux_dirent64 *)(buf + off));
+            off += dent->d_reclen
+        ) {
+            if (!strcmp(dent->d_name, ".") || !strcmp(dent->d_name, ".."))
+                continue;
+            if (statx(dir, dent->d_name, 0, STATX_BTIME | STATX_ATIME, &sbuf) < 0)
+                die("statx");
+            if (sbuf.stx_btime.tv_sec > last_christmas)
+                insert_letter(&letters, &letters_len, strdup(dent->d_name), sbuf.stx_btime.tv_sec);
+        }
+    }
+    qsort(letters, letters_len, sizeof(struct letter), cmp_letter);
+    for (int i=0; i < letters_len; i++) {
+        printf("%ld days:  %s\n", sec_to_days(next_christmas - letters[i].timestamp), letters[i].filename);
+    }
 }
