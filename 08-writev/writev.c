44290fa0744026664a43fc269238642c64d269df
Nour-eddine Taleb <contact@noureddine.xyz>
2023-04-05 21:51:05 +0000

    08-writev
    Signed-off-by: Nour-eddine Taleb <contact@noureddine.xyz>

--- 08-writev/writev.c
+++ 08-writev/writev.c
@@ -12,9 +12,22 @@
 
 #define die(msg) do { perror(msg); exit(EXIT_FAILURE); } while(0)
 
+int cmp(const struct iovec *a, const struct iovec *b) {
+    return strcmp(a->iov_base, b->iov_base);
+}
 
+#define IOV_BUF(iov) ((char *)iov.iov_base)
 int main() {
-    // FIXME: Read in lines (HINT: getline(3))
-    // FIXME: Shuffle lines (HINT: random(3))
-    // FIXME: Dump lines with writev(2)
+    struct iovec iov[512];
+    int i;
+    ssize_t len;
+    for (
+        i = 0;
+            i < 512 
+            && (len = getline((char **)&(iov[i].iov_base), &iov[i].iov_len, stdin)) > 0 
+            && (IOV_BUF(iov[i])[len] = 0, 1);
+        i++
+    );
+    qsort(iov, i, sizeof iov[0], (comparison_fn_t)cmp);
+    writev(1, iov, i);
 }
