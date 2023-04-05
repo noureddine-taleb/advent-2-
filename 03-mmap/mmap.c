7409a4c81254a94c55524d805771cf2fff5e95e9
Nour-eddine Taleb <contact@noureddine.xyz>
2023-04-05 21:51:04 +0000

    03-mmap
    Signed-off-by: Nour-eddine Taleb <contact@noureddine.xyz>

--- 03-mmap/mmap.c
+++ 03-mmap/mmap.c
@@ -1,79 +1,66 @@
-#include <sys/types.h>
-#include <sys/stat.h>
+#include <sys/mman.h>
+#include <unistd.h>
 #include <fcntl.h>
 #include <stdio.h>
-#include <errno.h>
-#include <unistd.h>
-#include <sys/mman.h>
 #include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
 
-// We define two CPP macros to hide the ugliness of these compiler
-// attributes.
-//
-// - section: This variable attribute pushes the variable into the
-//            named ELF section. Thereby, we let the linker collect
-//            variable definitions and concentrate them in one place
-//
-// - aligned: Normally, variables can be densely packed in the
-//            data/BSS section. However, as we want to replace parts
-//            of our data section with a file mapping, we obey the
-//            MMU's granularity (4096 bytes).
-#define persistent   __attribute__((section("persistent")))
-#define PAGE_SIZE 4096
+#define PAGE_SIZE 0x1000
+
+#ifndef PERSISTENCE_SECTION
+#define PERSISTENCE_SECTION "persistent"
+#endif
+
+#ifndef PERSISTENCE_FILE
+#define PERSISTENCE_FILE "mmap.persistent"
+#endif
+
+#define persistent __attribute__((section(PERSISTENCE_SECTION)))
 #define page_aligned __attribute__((aligned(PAGE_SIZE)))
 
-// This is a special variable that we use in setup_persistent to
-// locate the start of the persistent section. We page align it, such
-// that its address is placed at the beginning of a page. This is
-// equivalent to an HEX address ending in 3 zeros.
-//
-// For example: &persistent_start = 0x4b8000
-page_aligned persistent int persistent_start;
+#define PERSISTENT_START page_aligned persistent char __per_start__;
+#define PERSISTENT_END page_aligned persistent char __per_end__;
 
-// This is our actual persistent variable. We will use it as a
-// counter, how often the program was executed.
-persistent int foobar = 23;
+#define PAGE_ALIGN_DOWN(addr) ((unsigned long)addr & ~0xfff)
+#define PAGE_ALIGN_UP(addr) (((unsigned long)addr + 0xfff) & ~0xfff)
 
-// Although this variable is placed between persistent_{start,end}, it
-// does *NOT* have the persistent variable, whereby it is place not
-// within the persistent section.,
-int barfoo = 42;
+PERSISTENT_START
 
-// A second special variable that marks the end of the persistent
-// region. We also make it page aligned, whereby the linker will not
-// place other
-page_aligned persistent int persistent_end;
+persistent int i;
+persistent int j;
 
-int setup_persistent(char *fn) {
-    // FIXME: Install persistent mapping and return 0;
-    return -1;
+PERSISTENT_END
+
+extern char __start_persistent[];
+extern char __stop_persistent[];
+
+void setup_persistent(char *file)
+{
+	unsigned long	segment_start;
+	unsigned long	segment_end;
+	unsigned long	segment_len;
+    int 			fd;
+
+	segment_start = PAGE_ALIGN_DOWN(__start_persistent);
+	segment_end = PAGE_ALIGN_DOWN(__stop_persistent);
+	segment_len = segment_end - segment_start;
+
+    fd = open(file, O_CREAT | O_RDWR);
+    if (fd < 0)
+		perror("open"), exit(1);
+	ftruncate(fd, segment_len);
+	if (munmap((char *)segment_start, segment_len) < 0)
+		perror("munmap"), exit(1);
+	if (mmap((char *)segment_start, segment_len, PROT_READ | PROT_WRITE, MAP_FIXED | MAP_SHARED, fd, 0) == MAP_FAILED)
+		perror("mmap"), exit(1);
 }
 
-int main(int argc, char *argv[]) {
-    printf("section persistent: %p--%p\n", &persistent_start, &persistent_end);
-    // Install the persistent mapping
-    if (setup_persistent("mmap.persistent") == -1) {
-        perror("setup_persistent");
-        return -1;
-    }
-
-    // For foobar, we see that each invokation of the programm will
-    // yield an incremented result.
-    // For barfoo, which is *NOT* in the persistent section, we will
-    // always get the same result.
-    printf("foobar(%p) = %d\n", &foobar, foobar++);
-    printf("barfoo(%p) = %d\n", &barfoo, barfoo++);
-
-    {// This is ugly and you should not do this in production code.
-
-        // In order to see the memory mappings of the currently
-        // running process, we use the pmap (for process-map) tool to
-        // query the kernel (/proc/self/maps)
-        char cmd[256];
-        snprintf(cmd, 256, "pmap %d", getpid());
-        printf("---- system(\"%s\"):\n", cmd);
-        system(cmd);
-    }
-
-    return 0;
+int main()
+{
+	setup_persistent(PERSISTENCE_FILE);
+	printf("i = %d, j = %d\n", i, j);
+	i++;
+	j++;
+	exit(0);
 }
