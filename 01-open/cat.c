a3fb07878161496c8be36edd9e1c298df611c424
Nour-eddine Taleb <contact@noureddine.xyz>
2023-04-05 21:51:04 +0000

    01-open
    Signed-off-by: Nour-eddine Taleb <contact@noureddine.xyz>

--- 01-open/cat.c
+++ 01-open/cat.c
@@ -5,13 +5,21 @@
 #include <errno.h>
 #include <unistd.h>
 
-
+char buf[4096];
 int main(int argc, char *argv[]) {
     // For cat, we have to iterate over all command-line arguments of
     // our process. Thereby, argv[0] is our program binary itself ("./cat").
-    int idx;
+    int idx, ret, fd;
     for (idx = 1; idx < argc; idx++) {
-        printf("argv[%d] = %s\n", idx, argv[idx]);
+        fd = open(argv[idx], O_RDONLY);
+        if (fd < 0) {
+            fprintf(stderr, "no such file: %s\n", argv[idx]);
+            continue;
+        }
+        while ((ret = read(fd, buf, sizeof buf)) > 0) {
+            write(1, buf, ret);
+        }
+        close(fd);
     }
 
     return 0;
