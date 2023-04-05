bda65e8d2a5f8a6192c1a2eed5d64f38c9715b8b
Nour-eddine Taleb <contact@noureddine.xyz>
2023-04-05 21:53:07 +0000

    18-ptrace
    Signed-off-by: Nour-eddine Taleb <contact@noureddine.xyz>

--- 18-ptrace/systrace.c
+++ 18-ptrace/systrace.c
@@ -9,6 +9,7 @@
 #include <stdint.h>
 #include <sys/eventfd.h>
 #include <assert.h>
+#include <signal.h>
 #include <string.h>
 
 #define die(msg) do { perror(msg); exit(EXIT_FAILURE); } while(0)
@@ -21,9 +22,42 @@
 // which we got from PTRACE_GET_SYSCALL_INFO. This function also uses
 // the system call table to print pretty syscall names.
 void print_syscall(struct ptrace_syscall_info *info) {
-    printf("%s\n", names[__NR_write].name);
-    // FIXME: Handle info->op == PTRACE_SYSCALL_INFO_ENTRY
-    // FIXME: Handle info->op == PTRACE_SYSCALL_INFO_EXIT
+    if (info->op == PTRACE_SYSCALL_INFO_EXIT)
+        return;
+
+    struct syscall_name *sc = &names[info->entry.nr];
+    printf("%s(", sc->name);
+    for (int i=0; i < sc->argc; i++)
+        printf("%#llx, ", info->entry.args[i]);
+    printf(")\n");
+}
+
+pid_t pid;
+
+void start_proc(char **cmd) {
+    pid = fork();
+    
+    if (pid < 0)
+        die("fork");
+
+    if (pid > 0) {
+        // parent
+        int wstatus;
+        if (waitpid(pid, &wstatus, 0) < 0)
+            die("waitpid");
+
+        if (WIFEXITED(wstatus))
+            exit(WEXITSTATUS(wstatus));
+
+        ptrace(PTRACE_SETOPTIONS, pid, 0, PTRACE_O_TRACESYSGOOD);
+        return;
+    }
+
+    if (ptrace(PTRACE_TRACEME) < 0)
+        die("ptrace");
+
+    execvp(*cmd, cmd);
+    die("execvp");
 }
 
 int main(int argc, char *argv[]) {
@@ -31,10 +65,17 @@ int main(int argc, char *argv[]) {
         fprintf(stderr, "usage: %s CMD [ARGS...]\n", argv[0]);
         return -1;
     }
-    // FIXME/child: Issue PTRACE_TRACEME
-    // FIXME/child: execvp(argv[1], &argv[1])
-    // FIXME/parent: Wait for the first SIGTRAP
-    // FIXME/parent: set PTRACE_O_TRACESYSGOOD option
-    // FIXME/parent: PTRACE_SYSCALL to the next syscall
-    // FIXME/parent: Use PTRACE_GET_SYSCALL_INFO to get details
+    start_proc(&argv[1]);
+    while(1) {
+        ptrace(PTRACE_SYSCALL, pid, 0, 0);
+        int wstatus;
+        if (waitpid(pid, &wstatus, 0) < 0)
+            die("waitpid");
+        
+        if (WIFEXITED(wstatus))
+            exit(WEXITSTATUS(wstatus));
+        struct ptrace_syscall_info i;
+        ptrace(PTRACE_GET_SYSCALL_INFO, pid, sizeof i, &i);
+        print_syscall(&i);
+    }
 }
