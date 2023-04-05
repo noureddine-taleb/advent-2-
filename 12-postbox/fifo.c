0ef82356eb259670715f2291f72a155f3bb7209d
Nour-eddine Taleb <contact@noureddine.xyz>
2023-04-05 21:51:05 +0000

    12-postbox
    Signed-off-by: Nour-eddine Taleb <contact@noureddine.xyz>

--- 12-postbox/fifo.c
+++ 12-postbox/fifo.c
@@ -1,8 +1,21 @@
 
 int fifo_prepare(int epoll_fd) {
+    unlink("fifo");
+    mknod("fifo", S_IRUSR | S_IWUSR | S_IFIFO, 0);
+    int fd  = open("fifo", O_RDONLY|O_NONBLOCK);
+    epoll_add(epoll_fd, fd, EPOLLIN);
     printf("... by fifo:   echo 1 > fifo\n");
-    return -1;
+    return fd;
 }
 
 void fifo_handle(int epoll_fd, int fifo_fd, int events) {
+    if (events & EPOLLHUP)
+        return;
+        
+    char buf[256];
+    int  ret;
+    char banner[] = "fifo: ";
+    write(1, banner, sizeof banner);
+    ret = read(fifo_fd, buf, sizeof buf);
+    write(1, buf, ret);
 }
