1d8cf177f6eb6c9816e25db1078f014d9b0935c2
Nour-eddine Taleb <contact@noureddine.xyz>
2023-04-05 21:53:07 +0000

    19-perf
    Signed-off-by: Nour-eddine Taleb <contact@noureddine.xyz>

--- 19-perf/perf.c
+++ 19-perf/perf.c
@@ -26,20 +26,19 @@
 // PERF_FORMAT_ID are enabled).
 // Example (with id0=100, id1=200): {.nr = 2, .values = {{41433, 200}, {42342314, 100}}}
 typedef uint64_t perf_event_id; // For readability only
-struct read_format {
-    uint64_t nr;
+typedef struct {
+    uint64_t count;
     struct {
         uint64_t value;
         perf_event_id id; // PERF_FORMAT_ID
-    } values[];
-};
+    } list[3];
+} perf_event_val_t;
 
 // Structure to hold a perf group
 struct perf_handle {
     int group_fd;   // First perf_event fd that we create
     int nevents;    // Number of registered events
-    size_t rf_size; // How large is the read_format buffer (derived from nevents)
-    struct read_format *rf; // heap-allocated buffer for the read event
+    perf_event_val_t events_buf; // should be staticallly allocated
 };
 
 // Syscall wrapper for perf_event_open(2), as glibc does not have one
@@ -54,27 +53,73 @@ int sys_perf_event_open(struct perf_event_attr *attr,
 // the group leader. The function returns an id that can be used in
 // combination with perf_event_get.
 perf_event_id perf_event_add(struct perf_handle *p, int type, int config) {
+    if (p->nevents >= ARRAY_SIZE(p->events_buf.list))
+        die("surpassing the number of allowed events");
     // FIXME: Create event with perf_event_open
+    struct perf_event_attr attr = {
+        .type = type,
+        .config = config,
+        .size = sizeof(attr),
+        .disabled = 1,
+        .exclude_kernel = 1,
+        .exclude_hv = 1,
+        .read_format = PERF_FORMAT_GROUP | PERF_FORMAT_ID,
+    };
+    int fd = sys_perf_event_open(&attr, 0, -1, p->group_fd, 0);
+    if (fd < 0)
+        die("perf_event_open");
+    if (p->group_fd < 0)
+        p->group_fd = fd;
     // FIXME: Get perf_event_id with PERF_EVENT_IOC_ID
-    return -1;
+    __u64 id;
+    if (ioctl(fd, PERF_EVENT_IOC_ID, &id) < 0)
+        die("ioctl");
+    p->nevents++;
+    return id;
 }
 
 // Resets and starts the perf measurement
 void perf_event_start(struct perf_handle *p) {
     // FIXME: PERF_EVENT_IOC_{RESET, ENABLE}
+    if (ioctl(p->group_fd, PERF_EVENT_IOC_RESET, PERF_IOC_FLAG_GROUP) < 0)
+        die("ioctl");
+    if (ioctl(p->group_fd, PERF_EVENT_IOC_ENABLE, PERF_IOC_FLAG_GROUP) < 0)
+        die("ioctl");
 }
 
 // Stops the perf measurement and reads out the event
 void perf_event_stop(struct perf_handle *p) {
     // FIXME: PERF_EVENT_IOC_DISABLE
     // FIXME: Read event from the group_fd into an allocated buffer
+    if (ioctl(p->group_fd, PERF_EVENT_IOC_DISABLE, PERF_IOC_FLAG_GROUP) < 0)
+        die("ioctl");
+    if (read(p->group_fd, &p->events_buf, sizeof(p->events_buf)) < 0)
+        die("read");
+    if (p->nevents < p->events_buf.count)
+        die("read/overflow");
 }
 
 
 // After the measurement, this helper extracts the event counter for
 // the given perf_event_id (which was returned by perf_event_add)
 uint64_t perf_event_get(struct perf_handle *p, perf_event_id id) {
+    for (unsigned i = 0; i < p->events_buf.count; i++) {
+        if (p->events_buf.list[i].id == id) {
+            return p->events_buf.list[i].value;
+        }
+    }
+    die("no event");
     return -1;
+}
+
+void print_events(char *msg, uint64_t instr, uint64_t cycles, uint64_t cmisses) {
+    printf("%s    %ldM instr, %ldM cycles  %.2f ipc,  %ldM cache misses\n",
+        msg,
+        instr / (1000 * 1000),
+        cycles  / (1000 * 1000),
+        instr/((double)cycles),
+        cmisses / (1000 * 1000)
+    );
 }
 
 int main(int argc, char* argv[]) {
@@ -103,9 +148,43 @@ int main(int argc, char* argv[]) {
     //
     // "What Every Programmer Should Know About Memory", Ulrich Drepper, 2007,
     // https://www.akkadia.org/drepper/cpumemory.pdf
+    struct perf_handle ph_drepper = {
+        .group_fd = -1,
+    };
+    struct perf_handle ph_naive = {
+        .group_fd = -1,
+    };
 
+    perf_event_id inst = perf_event_add(&ph_drepper, PERF_TYPE_HARDWARE, PERF_COUNT_HW_INSTRUCTIONS);
+    perf_event_id cyc = perf_event_add(&ph_drepper, PERF_TYPE_HARDWARE, PERF_COUNT_HW_CPU_CYCLES);
+    perf_event_id miss = perf_event_add(&ph_drepper, PERF_TYPE_HARDWARE, PERF_COUNT_HW_CACHE_MISSES);
+    perf_event_start(&ph_drepper);
     matrixmul_drepper(dim, A, B, C0);
+    perf_event_stop(&ph_drepper);
+    inst = perf_event_get(&ph_drepper, inst);
+    cyc = perf_event_get(&ph_drepper, cyc);
+    miss = perf_event_get(&ph_drepper, miss);
+    print_events("drepper", 
+        inst,
+        cyc,
+        miss
+    );
+
+    inst = perf_event_add(&ph_naive, PERF_TYPE_HARDWARE, PERF_COUNT_HW_INSTRUCTIONS);
+    cyc = perf_event_add(&ph_naive, PERF_TYPE_HARDWARE, PERF_COUNT_HW_CPU_CYCLES);
+    miss = perf_event_add(&ph_naive, PERF_TYPE_HARDWARE, PERF_COUNT_HW_CACHE_MISSES);
+    perf_event_start(&ph_naive);
     matrixmul_naive(dim, A, B, C1);
+    perf_event_stop(&ph_naive);
+    inst = perf_event_get(&ph_naive, inst);
+    cyc = perf_event_get(&ph_naive, cyc);
+    miss = perf_event_get(&ph_naive, miss);
+    print_events("naive", 
+        inst,
+        cyc,
+        miss
+    );
+
 
     // Sanity Checking: are both result matrices equal (with a margin of 0.1%) ?
     for (unsigned i = 0; i < (dim*dim); i++) {
